5f4a6d8fdd9a977f76c12629e665fb0cdb0630cc
bisari <bisari31@gmail.com>
2023-05-30 22:26:38 +0900

    chore: svg 파일 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,17 @@
+import SunIcon from './sun.svg';
+import MoonIcon from './moon.svg';
+import GithubIcon from './github.svg';
+import SearchIcon from './search.svg';
+import MenuIcon from './menu.svg';
+import CloseIcon from './close.svg';
+import DocumentIcon from './document.svg';
+
+export {
+  SunIcon,
+  MoonIcon,
+  GithubIcon,
+  SearchIcon,
+  MenuIcon,
+  CloseIcon,
+  DocumentIcon,
+};
