159997045eadbac739b6e2afef3387a30fa0d093
bisari <bisari31@gmail.com>
2023-05-31 02:08:30 +0900

    feat: 다크모드 storage,os 동기화

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -1,17 +1,28 @@
 import { useEffect, useState } from 'react';
 
 export default function useTheme() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(
+    window.matchMedia('(prefers-color-scheme: dark)').matches,
+  );
 
   const handleToggleTheme = () => {
     setIsDarkMode((prev) => !prev);
   };
 
   useEffect(() => {
+    const isUserTheme = localStorage.getItem('color-theme');
+    if (isUserTheme) {
+      console.log(isUserTheme);
+      setIsDarkMode(isUserTheme === 'dark');
+    }
+  }, []);
+
+  useEffect(() => {
     document.documentElement.setAttribute(
       'data-theme',
       `${isDarkMode ? 'dark' : 'light'}`,
     );
+    window.localStorage.setItem('color-theme', isDarkMode ? 'dark' : 'light');
   }, [isDarkMode]);
 
   return { handleToggleTheme, isDarkMode };
