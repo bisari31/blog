efbb322185010c3b4ffca41f3f9487a4c2cd0e17
bisari <bisari31@gmail.com>
2023-07-12 17:25:52 +0900

    Revert "style: post link 태그 변경"
    This reverts commit ad456a1aad202b3ec7f8de5cb5065ce495bfe8e5.

--- src/components/post/PostPreview.tsx
+++ src/components/post/PostPreview.tsx
@@ -8,25 +8,21 @@ import Keywords from './Keywords';
 export default function PostPreview({ post }: { post: Post }) {
   return (
     <div>
-      <div className={styles.article}>
-        <div>
-          <Link href={{ pathname: post.url }} className={styles.contentWrapper}>
-            <time dateTime={post.date}>
-              {format(parseISO(post.date), 'LLL d, yyyy')}
-            </time>
-            <h2>{post.title}</h2>
-            <span>{post.description}</span>
-          </Link>
-          <Keywords keywords={post.keywords} />
+      <Link className={styles.article} href={{ pathname: post.url }}>
+        <div className={styles.contentWrapper}>
+          <time dateTime={post.date}>
+            {format(parseISO(post.date), 'LLL d, yyyy')}
+          </time>
+          <h2>{post.title}</h2>
+          <span>{post.description}</span>
         </div>
         {post.thumbnail && (
           <div className={styles.imgWrapper}>
-            <Link href={{ pathname: post.url }}>
-              <Image src={post.thumbnail} alt={post.title} fill />
-            </Link>
+            <Image src={post.thumbnail} alt={post.title} fill />
           </div>
         )}
-      </div>
+      </Link>
+      <Keywords keywords={post.keywords} />
     </div>
   );
 }
