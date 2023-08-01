2545eb8049a92ca5db815a0d633a4a2ea12db19f
bisari <bisari31@gmail.com>
2023-08-01 16:28:55 +0900

    feat: keyword post 많은 순으로 정렬

--- src/lib/contentlayer.ts
+++ src/lib/contentlayer.ts
@@ -1,17 +1,27 @@
 import { allPosts } from 'contentlayer/generated';
 
 export const sortedPosts = allPosts.sort((a, b) => {
-  const pathA = Number(a._raw.flattenedPath.split('-')[1]);
-  const pathB = Number(b._raw.flattenedPath.split('-')[1]);
+  const pathA = +a._raw.flattenedPath.split('-')[1];
+  const pathB = +b._raw.flattenedPath.split('-')[1];
   return pathB - pathA;
 });
 
-export const keywords = sortedPosts.reduce(
-  (acc: string[], cur) => {
-    cur.keywords?.forEach((keyword) => {
-      if (!acc.includes(keyword)) acc.push(keyword);
-    });
-    return acc;
-  },
-  ['All'],
-);
+const getSortedUniqueKeywords = (posts: Post[]) => {
+  const allKeywords: string[] = [];
+  posts.forEach((post) => allKeywords.push(...(post.keywords ?? '')));
+  const uniqueKeywords = [...new Set(allKeywords)];
+
+  const keywordCounts: { [key: string]: number } = {};
+
+  allKeywords.forEach(
+    (keyword) => (keywordCounts[keyword] = (keywordCounts[keyword] ?? 0) + 1),
+  );
+
+  const sortedKeywords = uniqueKeywords.sort(
+    (a, b) => keywordCounts[b] - keywordCounts[a],
+  );
+  sortedKeywords.unshift('All');
+  return sortedKeywords;
+};
+
+export const sortedUniqueKeywords = getSortedUniqueKeywords(allPosts);
