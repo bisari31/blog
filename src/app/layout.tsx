58048cd325e6ad2f4b91399e5d44fec01333b730
bisari <bisari31@gmail.com>
2023-08-17 11:37:33 +0900

    style: layout meta tag 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const notoSans = Noto_Sans_KR({
 
 export const metadata = {
   title: '이상원 기술 블로그',
-  description: '프론트엔드 개발자 이상원입니다.',
+  description: '프론트엔드 개발자 이상원의 기술 블로그입니다.',
   verification: {
     google: 'e-TNPhd8w4G89P7qHvpbNUMOzmwlO8jX-6zAin2jbK8',
     other: {
