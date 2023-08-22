a3a9797a5b8476388d388d934d0a62b842c57d6a
bisari <bisari31@gmail.com>
2023-08-22 22:58:30 +0900

    chore: sitemap 변경

---
+++ src/app/api/route.ts
@@ -0,0 +1,27 @@
+import { sortedPosts } from 'lib/contentlayer';
+
+export const GET = async () => {
+  const SITE_URL = process.env.SITE_URL;
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+	  <url>
+	    <loc>${SITE_URL}</loc>
+	  </url>
+    ${sortedPosts
+      .map(({ url }) => {
+        return `
+      <url>
+          <loc>${SITE_URL}${encodeURI(url)}</loc>
+      </url>
+    `;
+      })
+      .join('')}
+  }
+  </urlset>
+`;
+
+  return new Response(xml, {
+    status: 200,
+    headers: { 'Content-Type': 'text/xml' },
+  });
+};
