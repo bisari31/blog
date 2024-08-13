2459f7e540554639d39b59b1df2240d2547efda0
bisari31 <bisari31@gmail.com>
2024-08-13 16:27:40 +0900

    delete: redux 삭제

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,17 +1,5 @@
-import {
-  type TypedUseSelectorHook,
-  useDispatch,
-  useSelector,
-} from 'react-redux';
-import { AppDispatch, RootState } from 'redux/store';
-
-type DispatchFunc = () => AppDispatch;
 export { default as useFocus } from './useFocus';
 export { default as useKeyboardSelection } from './useKeyboardSelection';
 export { default as useModalOpen } from './useModalOpen';
 export { default as useOutsideClick } from './useOutsideClick';
 export { default as useScrollHidden } from './useScrollHidden';
-export { default as useTheme } from './useTheme';
-
-export const useAppDispatch: DispatchFunc = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/redux/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-
-import keywordSlice from './slices/keywordSlice';
-
-export const store = configureStore({ reducer: { keyword: keywordSlice } });
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
