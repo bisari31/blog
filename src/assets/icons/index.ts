aa8ad56257cd7894ae360ff11967d8e34b7c2213
bisari31 <bisari31@gmail.com>
2025-02-25 00:32:07 +0900

    feat: 헤더 이메일 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,3 @@
 export { default as Arrow } from './arrow.svg';
 export { default as GithubIcon } from './github.svg';
+export { default as Email } from './mail.svg';
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { GithubIcon } from 'assets/icons';
+import { Email, GithubIcon } from 'assets/icons';
 import { title } from 'constants/metadata';
 import Link from 'next/link';
 import { useKeywordActions } from 'stores/keyword-store';
@@ -20,13 +20,25 @@ export default function Header() {
           >
             {title}
           </Link>
-          <Link
-            aria-label="github link"
-            href="https://github.com/bisari31"
-            target="_blank"
-          >
-            <GithubIcon width={24} height={24} className="fill-gray-600" />
-          </Link>
+          <div className="flex items-center gap-3">
+            <Link
+              aria-label="email:bisari31@gmail.com"
+              href="mailto:bisari31@gmail.com"
+            >
+              <Email
+                width={32}
+                height={32}
+                className="fill-gray-600 stroke-white"
+              />
+            </Link>
+            <Link
+              aria-label="github link"
+              href="https://github.com/bisari31"
+              target="_blank"
+            >
+              <GithubIcon width={24} height={24} className="fill-gray-600" />
+            </Link>
+          </div>
         </div>
       </div>
     </header>
