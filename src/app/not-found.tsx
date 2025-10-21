eefaf0a200e2a49cf0eeca8e9a8f976f3f54cde8
bisari31 <bisari31@gmail.com>
2025-10-21 21:46:23 +0900

    feat: update not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,25 +1,8 @@
-'use client';
-
-import { Arrow } from 'assets/icons';
-import { useRouter } from 'next/navigation';
-
 export default function NotFound() {
-  const router = useRouter();
-
   return (
     <div className="flex flex-1 flex-col items-center justify-center text-gray-600">
       <p className="text-[100px] font-black sm:text-[120px]">404</p>
       <p className="text-2xl font-black sm:text-2xl">Page Not Found</p>
-      <button
-        className={
-          'mt-7 flex items-center gap-1 rounded-full border-2 bg-white px-4 py-2 font-medium sm:px-8 sm:py-3'
-        }
-        type="button"
-        onClick={() => router.back()}
-      >
-        <Arrow />
-        <span>Go Home</span>
-      </button>
     </div>
   );
 }
